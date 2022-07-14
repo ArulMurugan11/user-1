5efd6ce4b830cb5b8dd92261eead8276020ae0a1
Arulmurugan-BCT <arulmurugan.s@bahwancybertek.com>
2022-07-14 12:13:06 +0530

    aDded extra space

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,6 +6,6 @@ const authenticate=require('../middlewares/auth')
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
-router.get("/user-profile",authenticate, userController.userprofile);
+router.get("/user-profile", authenticate, userController.userprofile);
 
 module.exports = router;
